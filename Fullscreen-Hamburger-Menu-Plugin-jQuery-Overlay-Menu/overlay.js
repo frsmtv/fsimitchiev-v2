8b013c219b0fda1d854825c80b4b387dcb3b51ab
frsmtv <fsimitchiev@gmail.com>
2017-04-06 19:11:55 +0200

    oooo

--- Fullscreen-Hamburger-Menu-Plugin-jQuery-Overlay-Menu/overlay.js
+++
@@ -1,4 +0,0 @@
-$('#toggle').click(function() {
-    $(this).toggleClass('toggle-active');
-    $('#overlay').toggleClass('nav-active');
-});
--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
 
-    // **** RECENT -> ACCORDION ****
-    $('.accordion').accordion({
-        "transitionSpeed": 400
-    });
-
-
-
 
 });
